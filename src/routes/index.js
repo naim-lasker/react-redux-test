b535d56f955ba9983edb47b32b2c2746cc2d22b7
Naim Lasker <naimlasker@lie.com>
2019-09-30 22:38:05 +0600

    route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 import history from '../utils/history'
 import Home from '../pages/Home';
+import About from '../pages/About';
 
 class Routes extends Component {
   render() {
     return (
       <Router history={history}>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
         <Switch>
           <Route exact path='/' component={Home} />
-          {/* <Redirect path='/about' component={About} /> */}
+          <Route exact path='/about' component={About} />
+          {/* <Redirect  component={About} /> */}
         </Switch>
       </Router>
     );
