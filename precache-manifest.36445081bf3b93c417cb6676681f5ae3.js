3f38b7b322a5aca5f3fd6c577b591cbed07a66ef
Naim Lasker <naimlasker@lie.com>
2019-09-30 22:56:51 +0600

    Updates

---
+++ precache-manifest.36445081bf3b93c417cb6676681f5ae3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602612f289d8a2e87d55e8091bdb2144",
+    "url": "/react-redix-test-build/index.html"
+  },
+  {
+    "revision": "8b131bf003753b843823",
+    "url": "/react-redix-test-build/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "9b72937469477ac7b376",
+    "url": "/react-redix-test-build/static/js/2.44ff9b0b.chunk.js"
+  },
+  {
+    "revision": "8b131bf003753b843823",
+    "url": "/react-redix-test-build/static/js/main.d9898c94.chunk.js"
+  },
+  {
+    "revision": "641cbeadcaf5503be978",
+    "url": "/react-redix-test-build/static/js/runtime-main.03b282fa.js"
+  }
+]);
